4a3a40f8deeb8661b2c33467692c607c92d2a0b0
Matt Shepherd <matt.c.shepherd@gmail.com>
2016-04-22 10:48:37 -0700

    changed to using low gpio setting
    The relay switch I was using is actually GPIO Low to trigger.

--- python/toggle_garage_door.py
+++ python/toggle_garage_door.py
@@ -3,6 +3,6 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-GPIO.output(18,GPIO.HIGH)
-time.sleep(.5)
 GPIO.output(18,GPIO.LOW)
+time.sleep(.5)
+GPIO.output(18,GPIO.HIGH)
